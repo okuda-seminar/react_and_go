ccec77e10189de168d0eb46a83c8489b7dfebc0f
kasturada1 <kazuki1.katsurada@gmail.com>
2022-12-18 14:15:29 +0900

    Add other pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import EditMovie from './components/EditMovie';
 import ErrorPage from './components/ErrorPage';
+import Genres from './components/Genres';
+import GraphQL from './components/GraphQL';
 import Home from './components/Home';
+import Login from './components/Login';
+import ManageCatalogue from './components/ManageCatalogue';
 import Movies from './components/Movies';
 
 const router = createBrowserRouter([
@@ -16,7 +21,27 @@ const router = createBrowserRouter([
       {
         path: "/movies",
         element: <Movies />,
-      }
+      },
+      {
+        path: "/genres",
+        element: <Genres />,
+      },
+      {
+        path: "/admin/movie/0",
+        element: <EditMovie />,
+      },
+      {
+        path: "/manage-catalogue",
+        element: <ManageCatalogue />,
+      },
+      {
+        path: "/graphql",
+        element: <GraphQL />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
     ]
   }
 ])
