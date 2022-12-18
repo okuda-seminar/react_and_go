387698a54f4a836da5e7abb560d8a30053740263
kasturada1 <kazuki1.katsurada@gmail.com>
2022-12-18 13:37:37 +0900

    Set up a new project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
-    <div className="container">
-      <div className="row">
-        <div className="col">
-        <App msg="State using functional component!"/>
-        </div>
-      </div>
-    </div>
+    <App />
   </React.StrictMode>
 );
